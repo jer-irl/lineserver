ccb028d01fe8074fc75110813a4a353acd58539d
Jeremy Schroeder <jpschroeder2014@gmail.com>
2018-08-31 12:54:38 -0400

    System tests work

--- test/system_test.py
+++ test/system_test.py
@@ -8,7 +8,7 @@ from lineserver.server import Server
 class SystemTestCase(unittest.TestCase):
     def setUp(self):
         self.port = 8000
-        self.server = Server(self.port, '../frankenstein.txt')
+        self.server = Server(self.port, './frankenstein.txt')
         server_thread = threading.Thread(target=self.server.serve_forever)
         server_thread.start()
 
@@ -25,14 +25,16 @@ class SystemTestCase(unittest.TestCase):
         request = urllib.request.Request('http://127.0.0.1:{}/lines/1435'.format(self.port), method='GET')
         response = urllib.request.urlopen(request)
         self.assertEqual(int(response.status), 200)
-        self.assertEqual(response.read().strip(), "The astonishment which I had at first experienced on this discovery")
+        self.assertEqual(str(response.read(), encoding='utf-8').strip(),
+                         "The astonishment which I had at first experienced on this discovery")
 
     def test_empty_line(self):
-        request = urllib.request.Request('http://127.0.0.1:{}/lines/1'.format(self.port), method='GET')
-        response = urllib.request.urlopen(request)
-        self.assertEqual(response.read().strip(), "")
+        response = urllib.request.urlopen('http://127.0.0.1:{}/lines/1'.format(self.port))
+        self.assertEqual(int(response.status), 200)
+        self.assertEqual(repr(str(response.read(), encoding='utf-8').strip()), r"'\ufeff'")
 
     def test_past_the_end(self):
         request = urllib.request.Request('http://127.0.0.1:{}/lines/8000'.format(self.port), method='GET')
-        response = urllib.request.urlopen(request)
-        self.assertEqual(int(response.status), 413)
+        with self.assertRaises(urllib.request.HTTPError) as cm:
+            urllib.request.urlopen(request)
+        self.assertEqual(int(cm.exception.code), 413)
