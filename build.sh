2f8f39325146806a666173661658a13e0083baf2
Jeremy Schroeder <jpschroeder2014@gmail.com>
2018-09-01 10:15:51 -0400

    Made server more extensible

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python3 -m unittest test/system_test.py
+python3 -m unittest test/unit_test.py
--- lineserver/server.py
+++ lineserver/server.py
@@ -9,7 +9,14 @@ class _Handler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         print("Received GET request")
-        line_number = self.get_line_number()
+        endpoint = self.path.split('/')[1]
+        try:
+            self.get_endpoint_handlers[endpoint](self)
+        except KeyError as ex:
+            self.send_error(404, "No handler for endpoint", str(ex))
+
+    def handle_get_lines(self):
+        line_number = self._get_line_number()
         try:
             line = self.server.cache.get_bytes_for_line(line_number)
             self.send_response(200)
@@ -18,8 +25,11 @@ class _Handler(http.server.BaseHTTPRequestHandler):
         except LinePastEndError as ex:
             self.send_error(413, "{} is past the end of the file".format(line_number), str(ex))
 
+    get_endpoint_handlers = {
+        "lines": handle_get_lines
+    }
 
-    def get_line_number(self):
+    def _get_line_number(self):
         return int(self.path.split('/')[-1])
 
 
